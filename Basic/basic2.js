099d0f96b155c5476c12475bfed7b3117d2a4e17
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-12 13:58:18 +0530

    keyword

---
+++ Basic/basic2.js
@@ -0,0 +1,53 @@
+// var let const 
+
+// var a = 10;  // global scope
+
+// console.log("Outer value of a = ",a);
+
+// let b = 20; // block scope
+
+// console.log("outer value of b = ",b);
+
+
+// {
+//     console.log("Inner value of a = ",a);
+//     console.log("Inner value of b = ",b);
+    
+
+//     let c = 19;
+
+//     console.log("Inner value of c = ",c);
+    
+    
+// }
+
+// console.log("outer value of c = ",c);
+
+
+// var a = 10;
+
+// console.log("value of a = ",a);
+
+// var a = 20;
+
+// console.log("value of a = ",a);
+
+
+// let a = 10;
+// console.log("value of a = ",a);
+
+// a = 20; // reassign
+// console.log("value of a = ",a);
+
+const pi = 3.14;
+
+console.log("value of pi = ",pi);
+
+pi = 3.15;
+
+console.log("value of pi = ",pi);
+
+
+
+
+
