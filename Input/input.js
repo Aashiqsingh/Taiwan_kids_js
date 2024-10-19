d6e63383d14fae4fa753a9e7fd1bc071ef2f2459
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-19 11:33:25 +0530

    input

---
+++ Input/input.js
@@ -0,0 +1,19 @@
+// alert("Hello students...")
+
+// var x = confirm("Are you married?") // true or false
+
+// console.log(x);
+
+
+// var x = prompt("Enter your name")
+// console.log(x);
+
+var a = parseInt(prompt("Enter first value"))
+var b = parseInt(prompt("Enter second value"))
+
+// var a = 12
+// var b = 10
+
+var add = a + b;
+
+console.log("Addition = ",add);
---
+++ Operator/AirthmaticOpt.js
@@ -0,0 +1,27 @@
+// var a = 15;
+// var b = 12;
+
+// console.log("Addition = ",a+b);
+
+// console.log("Subtraction = ",a-b);
+
+// console.log("Multiplication = ",a*b);
+
+// console.log("Division = ",a/b);
+
+// console.log("Modulus = ",a%b);
+
+// var a = 3;
+// var b = 4;
+
+// var power = b ** a;
+// // a*a*a*a 
+// // 4*4*4
+// console.log("Exponent = ",power);
+
+
+var a = 5;
+
+// a = a + 4;
+// a += 4;
+console.log(a);
