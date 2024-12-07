5e02fc98d953dc4619154b7ea3ed44446fa06fba
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-12-07 11:37:40 +0530

    forloop , while loop

---
+++ Loop/forLoop.js
@@ -0,0 +1,50 @@
+// Why loops is using : for iteration many times
+
+// 1 - 100 : 
+
+// for loop : Three stages 
+//     1. intialization
+//     2. condition 
+//     3. increment or decrement
+    // i++ = > 1 + 1 = 2 
+
+// for(let i=1;i<=100;i++){
+//     console.log(i);
+    
+// }
+
+// for(let i=1;i<=100;i++){
+
+//     // console.log(i);
+
+//     if(i%2==0)
+//     {
+//         console.log(i);
+        
+//     }
+    
+// }
+
+
+// for(let i=1;i<=100;i++){
+
+//     // console.log(i);
+
+//     if(i%2==1)
+//     {
+//         console.log(i);
+        
+//     }
+    
+// }
+
+// 2 * 1 = 2
+// 2 * 2 = 4
+
+// var num = 11;
+
+// for(let i=1;i<=10;i++)
+// {
+//     console.log(num + "*" + i + "=" +  num*i);
+    
+// }
---
+++ Loop/whileLoop.js
@@ -0,0 +1,45 @@
+
+
+
+// for(let i=0;i<100;i++){
+//     console.log(i);
+    
+// }
+
+// let i=1;
+// while(i<=50)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+// console.log(12/5);
+// console.log(Math.floor(12/5));
+
+
+// let num = 7658; // reverse number
+// let flag = num;
+// var rev = 0;
+// while(num!=0)
+// {
+//     let rem = num % 10;
+//     rev = rev * 10 + rem;
+//     num = Math.floor(num / 10);
+// }
+
+// console.log(rev);
+
+// if(flag ==rev)
+// {
+
+// }
+
+
+// pallindrome number
+
+var a = "10"
+var b = "10";
+
+console.log(a == b); // true
+console.log(a === b); // true
+
