da271b0008c4bb221d5845c1e8c216183a914fad
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-12-14 11:45:08 +0530

    string methods

---
+++ IfElse/ternary.js
@@ -0,0 +1,21 @@
+var a = 10;
+var b = 11;
+
+// var ans = a > b ? "A is gretter" : "B is gretter";
+// console.log(ans);
+
+// var a = 40;
+// var b = 15;
+// var c = 12;
+
+
+// var FindMax = a > b ? (a > c ? a : c) : (b > c ? b : c);
+
+// console.log(FindMax , " is bigger");
+
+
+
+var a = 12;
+var b = 16;
+
+a < b ? console.log("A is smaller") : console.log("B is smaller");
---
+++ String/StringDemo1.js
@@ -0,0 +1,68 @@
+
+var str2 = "Dravya naisha taksh"
+
+
+// console.log(str);
+// console.log(str.length);
+
+
+// var x = str.charAt(4)
+// console.log(x);
+
+// var x = str.charCodeAt(0)
+
+// var x = str.concat(str2)
+// console.log(x);
+
+
+// var x = str.endsWith(".")
+// console.log(x);
+
+
+// var x = str.startsWith("A")
+// console.log(x);
+
+// var x = str.includes("pvt")
+// console.log(x);
+
+// var x = str.repeat(10)
+// console.log(x);
+
+// var x = str.replace("soft","hard")
+// console.log(x);
+
+// var x = str.replace("o","z")
+// var x = str.replaceAll("o","z")
+
+// console.log(x);
+var str = "Aoyal technosoft pvt ltd";
+
+// var x = str.slice(6,10)
+// console.log(x);
+
+// var x = str.split(" ")
+// var x = str.split("")
+// var x = str.split("o")
+
+// console.log(x);
+
+
+// var x = str.toUpperCase()
+var x = str.toLocaleLowerCase()
+
+console.log(x);
+
+
+
+
+
+// console.log(x); // ascii value 
+
+
+
+user :- naishamehta
+user :- takshshah
+
+
+output :- @naishamehta11
+output :- @takshshah9
