1a6308573b0cabf108ce5ec84d804905739e531b
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-01 11:36:44 +0530

    array

---
+++ Array/Array1.js
@@ -0,0 +1,30 @@
+// what is array?
+// Array is a collection of dataTypes 
+
+// var eng = 67;
+// var math = 87;
+// var 
+
+
+var Marks = [67,98,88,56,59,90];
+
+console.log(Marks);
+
+// Array method or function are eqivalent ()
+
+// console.log(Marks.length);
+
+// console.log(Marks[0]);
+// console.log(Marks[1]);
+
+
+for(let i=0;i<Marks.length;i++)
+{
+    console.log(Marks[i]);
+    
+}
+
+
+
+
+
---
+++ Array/Array2.js
@@ -0,0 +1,60 @@
+// var names = ["varun","naisha","taksh","dravya","roshan","hiya","diya"];
+
+// var arr = [45,74,86,34,64];
+
+// console.log(names);
+// console.log(names[0]);
+// console.log(names[1].toUpperCase());
+
+// var x = names.concat(arr)
+// var x = arr.concat(names)
+// console.log(x);
+
+
+// names.push("aashiq") // push work with last index
+// names.push("aashiq","seenu","aarti")
+// console.log(names);
+
+
+// names.pop();
+// names.pop();
+// console.log(names);
+
+
+
+// names.unshift("aashiq")
+// console.log(names);
+
+
+// names.shift()
+// names.shift()
+// console.log(names);
+
+
+// var x = names.includes("siya")
+// console.log(x);
+
+// var x = names.indexOf("hiya")
+// console.log(x);
+
+// var x = names.join(" ")
+// var x = names.join("")
+// var x = names.join("$")
+
+// console.log(x);
+
+
+// var x = names.reverse()
+// console.log(x);
+
+var names = ["varun","naisha","taksh","dravya","roshan","hiya","diya"];
+
+
+
+// var x = names.slice(0)
+// var x = names.slice(0,4)
+
+// console.log(x);
+
+
+
