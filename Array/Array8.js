d90dd50bd2d86dd3ed97853f34df07db869abf96
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-29 11:37:56 +0530

    json array , reduce

---
+++ Array/Array8.js
@@ -0,0 +1,10 @@
+// var name = prompt("Enter your name ");
+
+// // console.log(name);
+// var reverseName = name.split("").reverse().join("");
+// console.log(reverseName);
+
+
+var animal = ["dog","cat","sheep","elephant","tiger","lion","cow","goat"];
+
+// delete --> sheep 
---
+++ JsonArray/Array1.js
@@ -0,0 +1,61 @@
+var students = [
+    {
+        roll:1,
+        name:"Naisha",
+        email:"naisha@gmail.com",
+        age:14,
+        gender:"female"
+    },
+    {
+        roll:2,
+        name:"Taksh",
+        email:"taksh@gmail.com",
+        age:15,
+        gender:"male"
+    },
+    {
+        roll:3,
+        name:"Dravya",
+        email:"dravya@gmail.com",
+        age:11,
+        gender:"male"
+    }
+]
+
+// var x = students.map((stu)=>{
+//     return stu.name
+// })
+// console.log(x);
+
+// var newArr = []
+// students.forEach((stu)=>{
+//     // console.log(stu.name);
+//     newArr.push(stu.name)
+    
+// })
+
+// console.log(newArr);
+
+
+
+
+// console.log(students);
+// console.log(students[0]);
+// console.log(students[0].name);
+
+
+// var maleGender = students.filter((stu)=> stu.gender == "male")
+// console.log(maleGender);
+
+
+// var age = students.filter((stu)=> stu.age > 12)
+// console.log(age);
+
+// students.forEach((stu)=>{
+//     if(stu.age > 12)
+//     {
+//         console.log(stu);
+//     }
+// })
+
+
