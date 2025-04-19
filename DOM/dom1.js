ac20f1ba7369bf8f9c3b00674e7b4120010bb0fe
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-04-19 11:34:54 +0530

    dom manipulation

---
+++ DOM/dom1.js
@@ -0,0 +1,13 @@
+// document.getElementById("txt").innerHTML = "Royal technosoft pvt ltd."
+
+
+function changeTxt(){
+
+    const txt = document.getElementById("txt")
+    txt.innerHTML = "Royal technosoft pvt ltd."
+    txt.style.backgroundColor = "red"
+    txt.style.color = "white"
+    txt.style.height = "200px"
+    txt.style.width = "200px"
+
+}
